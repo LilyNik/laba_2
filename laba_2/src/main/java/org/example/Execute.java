7bd8aaf2115bc680a9c78ebc11c1c961afc8c341
LilyNik <lilyzazylil@gmail.com>
2024-12-06 16:28:20 +0300

    Update Execute.java
    Метод infixToPostfix выполняет следующие задачи:
    
    Преобразует инфиксные выражения в постфиксную нотацию, соблюдая порядок операций и корректность скобок.
    Помогает упростить дальнейшие вычисления с использованием постфиксной нотации, которая легко разбирается для последующих операторов в вычислениях.

--- laba_2/src/main/java/org/example/Execute.java
+++ laba_2/src/main/java/org/example/Execute.java
@@ -30,4 +30,46 @@ public class Execute {
             instance = new Execute();
         return instance;
     }
+
+    String infixToPostfix(String infixStr) {
+        char[] chars = infixStr.toCharArray();
+        String postfixStr = "";
+        Stack<Character> stack = new Stack<>();
+        int index = 0;
+
+        while (index < chars.length) {
+            char chr = chars[index];
+
+            if (chars[index] == '.') {
+                checkForValidDecimalPoint(index, chars);
+            } else if (Character.isDigit(chr) || Character.isLetter(chr) || chr == '_') {
+                int leftBorder = leftBorder(index, chars);
+                String varOrNum = "";
+                for (int ind = index; ind <= index + leftBorder; ind++) {
+                    varOrNum += chars[ind];
+                }
+                postfixStr += varOrNum + " ";
+
+                index += leftBorder;
+            } else if (chr == '(') {
+                stack.push('(');
+            } else if (chr == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(')
+                    postfixStr += stack.pop();
+                stack.pop();
+            } else if (isOperator(chr)) {
+                while (!stack.isEmpty() && PRIORITY.get(stack.peek()) >= PRIORITY.get(chr))
+                    postfixStr += stack.pop();
+                stack.push(chr);
+            }
+
+            index++;
+        }
+
+        while (!stack.isEmpty()) {
+            postfixStr += stack.pop();
+        }
+
+        return postfixStr.trim(); // Удаляем лишний пробел в конце
+    }
 }
